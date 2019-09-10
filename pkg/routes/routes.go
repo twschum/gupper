beaeb1821353ef9ac768ff6f7684060e3d29d194
Tim Schumacher <tim.schumacher@nokia.com>
2019-09-10 17:32:08 -0400

    Add routes package for URLs

---
+++ pkg/routes/routes.go
@@ -0,0 +1,28 @@
+/*
+
+Constants file for the route methods
+
+*/
+package routes
+
+import (
+	"fmt"
+)
+
+const (
+	LATEST   = "/latest"
+	DOWNLOAD = "/download"
+	UPLOAD   = "/upload"
+)
+
+func Latest(url *string) (route string) {
+	return *url + LATEST
+}
+
+func Download(url *string) (route string) {
+	return *url + DOWNLOAD
+}
+
+func Upload(url *string) (route string) {
+	return *url + UPLOAD
+}
