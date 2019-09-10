802bac8aa72850c1d0bf48b3aa9d5460b9204c8a
Tim Schumacher <tim.schumacher@nokia.com>
2019-09-10 17:32:38 -0400

    Update with working get to /latest

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -5,11 +5,19 @@ Self-updating app, using a companion http server
 package main
 
 import (
-	"github.com/twschum/gupper/pkg/update"
+	"flag"
 	"fmt"
+
+	"github.com/twschum/gupper/pkg/update"
 )
 
+var updateUrl = flag.String("url", "localhost", "http update server address")
+var updatePort = flag.String("port", ":8080", "http update server port")
+// TODO https
+
 func main() {
-	var version = update.Check()
+	flag.Parse()
+	var url = fmt.Sprintf("http://%s%s", *updateUrl, *updatePort)
+	var version = update.Check(&url)
 	fmt.Printf("app version: %v\ndoing useful work now...\n", version)
 }
--- pkg/update/update.go
+++ pkg/update/update.go
@@ -6,19 +6,61 @@ Module handles updating the client from the server
 package update
 
 import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/twschum/gupper/pkg/routes"
 )
 
-// Version is just an int for now, could easily be replaced by a semantic
+// Version is just a number for now, could easily be replaced by a semantic
 // version object that could handle more realistic use-case
-type Version int
+type Version float64
 
-func Check() (current Version) {
-	current = 0
+func ParseVersion(s string) (Version, error) {
+	v, err := strconv.ParseFloat(s, 64)
+	return Version(v), err
+}
+
+
+// embedded in the code here for now,
+// maybe as a command line argument later at build time
+const current Version = 1.0
+const bad Version = 0
+
+func Check(url *string) (latest Version) {
 	// get current version
 	// ask server if it needs to update
-	// download the update
-	// extract and install the update, replacing current app
-	// exec to new version
+	latest, err := latestVersion(url)
+	if err != nil {
+		return current
+	}
+	if latest > current {
+		// download the update
+		// extract and install the update, replacing current app
+		// exec to new version
+	}
 	// return version
-	return
+	return latest
+}
+
+func latestVersion(url *string) (latest Version, err error) {
+	res, err := http.Get(routes.Latest(url))
+	if err != nil {
+		log.Printf("ERROR: problem contacting update server: %v", err)
+		return bad, err
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Printf("ERROR: problem reading response body: %v", err)
+		return bad, err
+	}
+	latest, err = ParseVersion(string(body))
+	if err != nil {
+		log.Printf("ERROR: problem parsing version from response body: %v", err)
+		return bad, err
+	}
+	return latest, err
 }
