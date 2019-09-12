1a7ed0789def739212ba95021d40e6a1367e6fb9
Tim Schumacher <tim.schumacher@nokia.com>
2019-09-11 23:48:43 -0400

    Adds build script that uploads version coded, cross compiled binaries

---
+++ build_version.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+################################################################################
+
+version=$1
+if [[ -z "$version" ]]; then
+    echo "usage: $0 <version>"
+    exit 1
+fi
+package_name=app
+package=cmd/$package_name/$package_name.go
+
+platforms=("windows/amd64" "windows/386" "darwin/amd64" "linux/amd64")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name=$package_name'-'$version'-'$GOOS'-'$GOARCH
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -ldflags "-X main.BuildVersion=$version" -o $output_name $package
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution…'
+        exit 1
+    fi
+    echo $output_name
+    mv $output_name packages/
+done
